8f9790f0b311f71fef948e5a850a5edfba10f778
yuppiez <bluubs@DESKTOP-E9ECPKD.localdomain>
2020-07-19 23:40:25 -0700

    INDEX PAGE AND ARRAYS
    import big globe from codepen credit Donovan Hutchinson and Sidoruk Sergey https://codepen.io/donovanh/pen/GBIiv
    	display big globe on index.html and css/style.css and scripts/script.js
    create button 'capitals'
    	addEventListener to capitals redirect to /capital-quiz.html thru nodelist
    create country object in /script/script1.js
    	country.countryName = array with every official country
    country.capitalName = matching array to .countryName with every capital(s) of every country
    yeet

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelectorAll('button');
+
+btn[0].addEventListener('click', () => {
+  window.location.href = "capital-quiz.html";
+})
---
+++ scripts/script1.js
@@ -0,0 +1,299 @@
+
+const country= {
+  countryName: [    
+    'Afghanistan',
+   'Albania',
+   'Algeria',
+   'Andorra',
+   'Angola',
+   'Antigua and Barbuda',
+   'Argentina',
+   'Armenia',
+   'Australia',
+   'Austria',
+   'Azerbaijan',
+   'The Bahamas',
+   'Bahrain',
+   'Bangladesh',
+   'Barbados',
+   'Belarus',
+   'Belgium',
+   'Belize',
+   'Benin',
+   'Bhutan',
+   'Bolivia',
+   'Bosnia and Herzegovina',
+   'Botswana',
+   'Brazil',
+   'Brunei',
+   'Bulgaria',
+   'Burkina Faso',
+   'Burundi',
+   'Cabo Verde',
+   'Cambodia',
+   'Cameroon',
+   'Canada',
+   'Central African Republic',
+   'Chad',
+   'Chile',
+   'China',
+   'Colombia',
+   'Comoros',
+   'Congo, Democratic Republic of the',
+   'Congo, Republic of the',
+   'Costa Rica',
+   'Côte d’Ivoire',
+   'Croatia',
+   'Cuba',
+   'Cyprus',
+   'Czech Republic',
+   'Denmark',
+   'Djibouti',
+   'Dominica',
+   'Dominican Republic',
+   'East Timor',
+   'Ecuador',
+   'Egypt',
+   'El Salvador',
+   'Equatorial Guinea',
+   'Eritrea',
+   'Estonia',
+   'Eswatini',
+   'Ethiopia',
+   'Fiji',
+   'Finland',
+   'France',
+   'Gabon',
+   'The Gambia',
+   'Georgia',
+   'Germany',
+   'Ghana',
+   'Greece',
+   'Grenada',
+   'Guatemala',
+   'Guinea',
+   'Guinea-Bissau',
+   'Guyana',
+   'Haiti',
+   'Honduras',
+   'Hungary',
+   'Iceland',
+   'India',
+   'Indonesia',
+   'Iran',
+   'Iraq',
+   'Ireland',
+   'Israel',
+   'Italy',
+   'Jamaica',
+   'Japan',
+   'Jordan',
+   'Kazakhstan',
+   'Kenya',
+   'Kiribati',
+   'Korea, North',
+   'Korea, South',
+   'Kosovo',
+   'Kuwait',
+   'Kyrgyzstan',
+   'Laos',
+   'Latvia',
+   'Lebanon',
+   'Lesotho',
+   'Liberia',
+   'Libya',
+   'Liechtenstein',
+   'Lithuania',
+   'Luxembourg',
+   'Madagascar',
+   'Malawi',
+   'Malaysia',
+   'Maldives',
+   'Mali',
+   'Malta',
+   'Marshall Islands',
+   'Mauritania',
+   'Mauritius',
+   'Mexico',
+   'Micronesia',
+   'Moldova',
+   'Monaco',
+   'Mongolia',
+   'Montenegro',
+   'Morocco',
+   'Mozambique',
+   'Myanmar',
+   'Namibia',
+   'Nauru',
+   'Nepal',
+   'Netherlands',
+   'New Zealand',
+   'Nicaragua',
+   'Niger',
+   'Nigeria',
+   'North Macedonia',
+   'Norway',
+   'Oman',
+   'Pakistan',
+   'Palau',
+   'Panama',
+   'Papua New Guinea',
+   'Paraguay',
+   'Peru',
+   'Philippines',
+   'Poland',
+   'Portugal',
+   'Qatar',
+   'Romania',
+   'Russia',
+   'Rwanda',
+   'Saint Kitts and Nevis',
+   'Saint Lucia',
+   'Saint Vincent and the Grenadines',
+   'Samoa',
+   'San Marino',
+   'Sao Tome and Principe',
+   'Saudi Arabia',
+   'Senegal',
+   'Serbia',
+   'Seychelles',
+   'Sierra Leone',
+   'Singapore',
+   'Slovakia',
+   'Slovenia',
+   'Solomon Islands',
+   'Somalia',
+   'South Africa',
+   'Spain',
+   'Sri Lanka',
+   'Sudan',
+   'Sudan, South',
+   'Suriname',
+   'Sweden',
+   'Switzerland',
+   'Syria',
+   'Taiwan',
+   'Tajikistan',
+   'Tanzania',
+   'Thailand',
+   'Togo',
+   'Tonga',
+   'Trinidad and Tobago',
+   'Tunisia',
+   'Turkey',
+   'Turkmenistan',
+   'Tuvalu',
+   'Uganda',
+   'Ukraine',
+   'United Arab Emirates',
+   'United Kingdom',
+   'United States',
+   'Uruguay',
+   'Uzbekistan',
+   'Vanuatu',
+   'Vatican City',
+   'Venezuela',
+   'Vietnam',
+   'Yemen',
+   'Zambia',
+   'Zimbabwe'],
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   capitalName: ['Kabul',
+   'Tirana',
+   'Algiers',
+   'Andorra la Vella',
+   'Luanda',
+   `Saint John's`,
+   'Buenos Aires',
+ 
+ 'Yerevan',
+ 'Canberra',
+ 'Vienna','Baku','Nassau','Manama','Dhaka','Bridgetown','Minsk','Brussels','Belmopan','Porto-Novo','Thimphu',`Sucre (de jure),
+ La Paz (seat of government)`,'Sarajevo','Gaborone','Brasilia','Bandar Seri Begawan','Sofia','Ouagadougou',
+ 'Gitega','Praia','Phnom Penh','Yaounde','Ottawa','Bangui',`N'Djamena`,'Santiago','Beijing','Bogotá','Moroni','Kinshasa','Brazzaville','San Jose','Yamoussoukro','Zagreb','Havana','Nicosia','Prague','Copenhagen',`Djibouti (city)`,'Roseau',              
+ 'Santo Domingo',`Dili`,'Quito','Cairo','San Salvador',`Malabo (de jure),
+ Oyala (seat of government)`,'Asmara','Tallinn',`Mbabane (administrative),
+ Lobamba (legislative, royal)`,'Addis Ababa','Suva','Helsinki','Paris','Libreville','Banjul','Tbilisi','Berlin','Accra','Athens',`Saint George's`,'Guatemala City',
+ `Conakry`,`Bissau`,`Georgetown`,`Port-au-Prince`,`Tegucigalpa`,`Budapest`,`Reykjavik`,
+ `New Delhi`,`Jakarta`,`Tehran`,`Baghdad`,`Dublin`,`Jerusalem`,`Rome`,
+ `Kingston`,`Tokyo`,`Amman`,`Nur-Sultan`,`Nairobi`,`Tarawa`,`Pyongyang`,
+ `Seoul`,`Pristina`,`Kuwait City`,`Bishkek`,`Vientiane`,`Riga`,`Beirut`,
+ `Maseru`,`Monrovia`,`Tripoli`,`Vaduz`,`Vilnius`,`Luxembourg (city)`,`Antananarivo`,
+ `Lilongwe`,`Kuala Lumpur`,`Male`,`Bamako`,`Valletta`,`Majuro`,`Nouakchott`,
+ `Port Louis`,`Mexico City`,`Palikir`,`Chisinau`,`Monaco`,`Ulaanbaatar`,`Podgorica`,
+ `Rabat`,`Maputo`,`Naypyidaw`,`Windhoek`,`Yaren District `,`Kathmandu`,`Amsterdam`,
+ `Wellington`,`Managua`,`Niamey`,`Abuja`,`Skopje`,`Oslo`,`Muscat`,
+ `Islamabad`,`Ngerulmud`,`Panama City`,`Port Moresby`,`Asunción`,`Lima`,`Manila`,
+ `Warsaw`,`Lisbon`,`Doha`,`Bucharest`,`Moscow`,`Kigali`,`Basseterre`,
+ `Castries`,`Kingstown`,`Apia`,`San Marino`,`São Tomé`,`Riyadh`,`Dakar`,
+ `Belgrade`,`Victoria`,`Freetown`,`Singapore`,`Bratislava`,`Ljubljana`,`Honiara`,
+ `Mogadishu`,`Pretoria (administrative),
+ Cape Town (legislative),
+ Bloemfontein (judicial)`,`Juba`,`Madrid`,`Sri Jayawardenepura Kotte`,`Khartoum`,`Paramaribo`,
+ `Stockholm`,`Bern`,`Damascus`,`Taipei`,`Dushanbe`,`Dodoma`,`Bangkok`,
+ `Lomé`,`Nukuʻalofa`,`Port of Spain`,`Tunis`,`Ankara`,`Ashgabat`,
+ `Funafuti`,`Kampala`,`Kyiv `,`Abu Dhabi`,`London`,`Washington, D.C.`,`Montevideo`,`Tashkent`,
+ `Port Vila`,`Vatican City`,`Caracas`,`Hanoi`,`Sana'a`,`Lusaka`,`Harare`,
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ ],
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ };
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ alert(country.countryName[123]);
+ 
+ alert(country.capitalName[123]);
+ 
